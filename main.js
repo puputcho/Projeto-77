bb7391a4f43447874002497386a4eef49eaea6bf
puputcho <67434177+puputcho@users.noreply.github.com>
2022-06-30 14:55:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+first = "Pedrao";
+last = "Zaquia";
+nome = first.concat(last)
+console.log(nome)
+a = 10
+b = 25
+c = 2
+d = 0.4
+e = 0.9999999999999999
+f = 3
+g = 100
+h = 0.06
+i = 1.9
+j = 4
+console.log(a)
+console.log(b)
+console.log(a + b)
+console.log(a / b * a / a + b / a / b * a - d - e * c + c + c * f / g - h - i + a + e - d / j - a + d * a / j)
